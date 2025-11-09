fa7252732717c0b470311d80a2113c8cb4c068fb
calm1205 <nagi.moriyama@mntsq.com>
2025-11-09 15:00:03 +0900

    fix: filename

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     lib: {
       entry: "packages/index.ts",
       formats: ["es"],
+      fileName: "index",
     },
     rollupOptions: {
       external: ["vue", "tailwindcss", "@ark-ui/vue"],
