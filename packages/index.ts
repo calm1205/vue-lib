f97327bb476f75a88b3734067fab0a99f0b1aef7
calm1205 <nagi.moriyama@mntsq.com>
2025-11-09 14:17:42 +0900

    fix: export

--- packages/index.ts
+++ packages/index.ts
@@ -1,8 +1,10 @@
 import HelloWorld from "./components/HelloWorld.vue"
+import Accordion from "./components/Accordion.vue"
 import { add } from "./libs/add"
 
 /**
  * ここでexportしたものがpublish対象
  */
 export { HelloWorld }
+export { Accordion }
 export { add }
