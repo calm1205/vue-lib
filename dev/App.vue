0e4cc8bfb02af0fd298ccab7af89bfd0ca1d42ed
calm1205 <nagi.moriyama@mntsq.com>
2025-11-10 21:10:33 +0900

    feat: add tailwind theme

--- dev/App.vue
+++ dev/App.vue
@@ -1,18 +1,3 @@
-<script setup lang="ts">
-import { HelloWorld } from "../packages/components/index.ts"
-import { Accordion } from "../packages/components/index.ts"
-import { VueIcon } from "../packages/components/index.ts"
-import { add } from "../packages/libs/index.ts"
+<script setup lang="ts"></script>
 
-console.log(add(1, 2))
-</script>
-
-<template>
-  <main class="p-4">
-    <div class="flex items-center gap-4">
-      <VueIcon />
-      <HelloWorld msg="Vite + Vue" />
-    </div>
-    <Accordion />
-  </main>
-</template>
+<template></template>
